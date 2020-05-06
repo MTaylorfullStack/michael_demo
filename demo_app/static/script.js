8cfe7e4b303e029212175f9f91cf3b29661ad2f3
MTaylorfullStack <mtaylor@codingdojo.com>
2020-05-05 19:43:58 -0500

    first commit

---
+++ demo_app/static/script.js
@@ -0,0 +1,10 @@
+function playground(){
+    console.log("It worked!")
+    var name = "Michael"
+    var names = ["mitchell", "connor", "steve"]
+    var name_descriptions = {
+        'mitchell': "student",
+        "connor": "student"
+    }
+    console.log(name, names, name_descriptions)
+}
